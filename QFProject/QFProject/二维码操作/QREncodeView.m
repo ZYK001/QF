6fb0e805e387c559076822787423c0ae86eecf8f
赵英奎 <zhaoyingkui@1-3.local>
2015-12-04 11:22:10 +0800

    修改扫描页面坐标

--- QFProject/QFProject/二维码操作/QREncodeView.m
+++ QFProject/QFProject/二维码操作/QREncodeView.m
@@ -20,11 +20,11 @@
 
 -(void)createQREncodeImageView:(UIView *)supperView andStringContent:(NSString *)content;
 {
-    UILabel *lbContent=[[UILabel alloc]initWithFrame:CGRectMake(-10, -50, 300, 20)];
-    lbContent.textColor=[UIColor orangeColor];
-    lbContent.text=@"下载KK直播，别忘记点赞哦～";
+    UILabel *lbContent=[[UILabel alloc]initWithFrame:CGRectMake(-40, -40, 350, 20)];
+    lbContent.textColor=[UIColor blueColor];
+    lbContent.text=@"下载KK直播，别忘记推荐给你的朋友并哦～";
     [self addSubview:lbContent];
-    
+    lbContent.font=[UIFont systemFontOfSize:15];
     UIImageView *imgView=[[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 200, 200)];
     imgView.image=[QRCodeGenerator qrImageForString:content imageSize:200];
     self.backgroundColor=[UIColor whiteColor];
